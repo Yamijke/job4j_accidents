9a586c37588b4a196ef1ea767a0626cbd1e5754a
Yamijke <serhei.knyazev@gmail.com>
2024-05-18 15:17:58 +0300

    5. Form с агрегационными объектами [#305523]

--- src/main/java/ru/job4j/accidents/controller/AccidentController.java
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.AccidentType;
+import ru.job4j.accidents.model.Rule;
 import ru.job4j.accidents.service.AccidentService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,11 +28,18 @@ public class AccidentController {
         types.add(new AccidentType(2, "Car and human"));
         types.add(new AccidentType(3, "Car and  велосипед"));
         model.addAttribute("types", types);
+        List<Rule> rules = List.of(
+                new Rule(1, "Статья. 1"),
+                new Rule(2, "Статья. 2"),
+                new Rule(3, "Статья. 3")
+        );
+        model.addAttribute("rules", rules);
         return "createAccident";
     }
 
     @PostMapping("/saveAccident")
-    public String save(@ModelAttribute Accident accident) {
+    public String save(@ModelAttribute Accident accident, HttpServletRequest req) {
+        String[] ids = req.getParameterValues("rIds");
         accidentService.save(accident);
         return "redirect:/index";
     }
--- src/main/java/ru/job4j/accidents/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentMem.java
@@ -3,6 +3,7 @@ package ru.job4j.accidents.repository;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.AccidentType;
+import ru.job4j.accidents.model.Rule;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -14,9 +15,17 @@ public class AccidentMem implements AccidentMemInterface {
     private Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
 
     private AccidentMem() {
-        save(new Accident(0, "test", "test", "test", new AccidentType(1, "Two cars")));
-        save(new Accident(0, "test1", "test1", "test1", new AccidentType(2, "Car and human")));
-        save(new Accident(0, "test2", "test2", "test2", new AccidentType(3, "Car and  велосипед")));
+        Rule rule1 = new Rule(1, "Rule 1");
+        Rule rule2 = new Rule(2, "Rule 2");
+        Rule rule3 = new Rule(3, "Rule 3");
+
+        Set<Rule> rules1 = new HashSet<>(Arrays.asList(rule1, rule2));
+        Set<Rule> rules2 = new HashSet<>(Arrays.asList(rule2, rule3));
+        Set<Rule> rules3 = new HashSet<>(Arrays.asList(rule1, rule3));
+
+        save(new Accident(0, "test", "test", "test", new AccidentType(1, "Two cars"), rules1));
+        save(new Accident(0, "test1", "test1", "test1", new AccidentType(2, "Car and human"), rules2));
+        save(new Accident(0, "test2", "test2", "test2", new AccidentType(3, "Car and  велосипед"), rules3));
     }
 
     public static AccidentMem getInstance() {
@@ -43,7 +52,7 @@ public class AccidentMem implements AccidentMemInterface {
     @Override
     public boolean update(Accident accident) {
         return accidents.computeIfPresent(accident.getId(),
-                (id, oldAccident) -> new Accident(oldAccident.getId(), accident.getName(), accident.getText(), accident.getAddress(), accident.getType())) != null;
+                (id, oldAccident) -> new Accident(oldAccident.getId(), accident.getName(), accident.getText(), accident.getAddress(), accident.getType(), accident.getRules())) != null;
     }
 
     @Override
